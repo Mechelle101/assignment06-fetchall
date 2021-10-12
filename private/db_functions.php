ddd0265da5d9efdcd109eb296278aee501bf416d
mechelle presnell <mechellenpresnell@students.abtech.edu>
2021-10-12 16:01:12 -0400

    first commit, starting assignment 06

---
+++ private/db_functions.php
@@ -0,0 +1,37 @@
+<?php
+
+// function db_connect() {
+//     try {
+//         $connection = new PDO('mysql:host='.DB_SERVER.'; dbname='.DB_NAME, DB_USER, DB_PASS); 
+//         return $connection;
+//     }
+    
+//     catch(Exception $e) {
+//         echo $e->getMessage();
+//     }
+// }
+
+//from my first file
+function db_connect() {
+$connection = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+confirm_db_connect($connection);
+return $connection;
+}
+
+function confirm_db_connect($connection) {
+    if($connection->connect_errno){
+        $msg = "Database connection failed: ";
+        $msg .= $connection->connect_error;
+        $msg .= " (" . $connection->connect_errno . ")";
+        exit($msg);
+    }
+}
+
+function db_disconnect($connection){
+    if(isset($connection)) {
+        $connection->close();
+    }
+}
+
+?>
+
